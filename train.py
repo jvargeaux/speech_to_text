1c12924e19ae7ad7a038868ea89b1092a0f654dd
Jimmy Vargo <james@ayo.tokyo>
2023-09-13 16:44:14 +0900

    Add h5py for mfcc storage, fix librosa errors

--- train.py
+++ train.py
@@ -6,6 +6,24 @@ import torchaudio
 import librosa, librosa.display
 from pathlib import Path
 import sounddevice as sd
+import h5py
+import os
+
+
+class ProgressBar():
+    def __init__(self):
+        self.length = 30
+        self.fill = '█'
+        self.empty = '-'
+        print(f'Progress: {self.empty * self.length} | 0/0 | {0:.1f}%', end='\r')
+
+    def update(self, filled, total):
+        percentage = min([filled, total]) / total
+        filled_length = int(percentage * self.length)
+        empty_length = self.length - filled_length
+        print(f'Progress: {self.fill * filled_length}{self.empty * empty_length} | {filled}/{total} | {(percentage * 100):.1f}%', end='\r')
+        if percentage >= 1:
+            print()
 
 
 class SpeechToTextRNN():
@@ -14,10 +32,14 @@ class SpeechToTextRNN():
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         print(f'device: {self.device}')
 
+        # Set MFCC meta parameters
+        self.hop_length = 512  # number of samples to shift
+        self.n_fft = 2048  # number of samples per fft (window size)
+        self.n_mfcc = 13  # standard minimum
+
         self.download_dataset()
 
     def download_dataset(self):
-        print('downloading dataset...')
         data_path = Path('data')
         if not data_path.exists():
             data_path.mkdir(parents=True)
@@ -27,55 +49,42 @@ class SpeechToTextRNN():
     def show_test_data(self, waveform=False, spectrogram=False, mfcc=False, play=False):
         if self.data is None:
             return
-        test = self.data.__getitem__(0)  # maybe this is questionable?
-        samples = test[0].numpy()
-        sr = test[1]
+        test = self.data.__getitem__(0)
+        samples, sample_rate, transcript, speaker_id, chapter_id, utterance_id = test
+        samples = samples.numpy()
 
-        print('-- Showing Test Data --')
-        print(test)
+        print('-- Sample Data --')
         print(f'{samples=}')
-        print(np.shape(samples))
-        print(f'{sr=}')
-        duration = librosa.get_duration(y=samples, sr=sr)
+        print(f'samples shape: {np.shape(samples)}')
+        print(f'{sample_rate=}')
+        print(f'{transcript=}')
+        print(f'{speaker_id=}')
+        print(f'{chapter_id=}')
+        print(f'{utterance_id=}')
+        # duration = librosa.get_duration(y=samples, sr=sample_rate)
+        duration = len(samples[0]) / sample_rate
         print(f'{duration=}')
 
-        hop_length = 512  # number of samples to shift
-        n_fft = 2048  # number of samples per fft (window size)
-        n_mfcc = 13  # standard minimum
-
-        # Samples in test file: 175,920
-        # Hop length: 512
-        # Number of frames: 175,920 / 512 = 343.59 (344)
-        # Shape after stft: (number of channels, 1 + (n_fft / 2), number of frames)
+        # # frames = # samples / hop length
+        # Shape after stft: (# channels, 1 + (n_fft / 2), # frames)
 
         if play is True:
             # need to transpose array from one row [1][n] to one column [n, 1] (one channel)
-            sd.play(data=samples.T, samplerate=sr)
-            # sd.wait()
+            sd.play(data=samples.T, samplerate=sample_rate)
+            sd.wait()
 
         if waveform is True:
-            librosa.display.waveshow(samples, sr=sr)
+            librosa.display.waveshow(samples, sr=sample_rate)
             plt.xlabel('Time')
             plt.ylabel('Amplitude')
             plt.show()
 
         if spectrogram is True:
-            # Fixed!
-            # Shape changes after stft: (1, 175920) -> (1, 1025, 344)
-            # We don't need num channels, since audio is mono.
-            # Remove first dimension: (1, 1025, 344) -> (1025, 344)
-            # Should work now with no error.
-            stft = librosa.core.stft(samples, hop_length=hop_length, n_fft=n_fft)
-            stft = stft[0]
-            print('stft shape')
-            print(np.shape(stft))
-
+            stft = librosa.core.stft(samples, hop_length=self.hop_length, n_fft=self.n_fft)
+            stft = stft[0]  # Remove first dimension (mono channel)
             spectrogram = np.abs(stft)
             log_spectrogram = librosa.amplitude_to_db(spectrogram)
-            print('spectrogram shape')
-            print(np.shape(log_spectrogram))
-
-            librosa.display.specshow(log_spectrogram, sr=sr, hop_length=hop_length)
+            librosa.display.specshow(log_spectrogram, sr=sample_rate, hop_length=self.hop_length)
             plt.xlabel('Time')
             plt.ylabel('Frequency')
             plt.colorbar()
@@ -83,71 +92,74 @@ class SpeechToTextRNN():
 
         if mfcc is True:
             # Same as stft, remove first dimension
-            mfccs = librosa.feature.mfcc(y=samples, n_fft=n_fft, hop_length=hop_length, n_mfcc=n_mfcc)
-            mfccs = mfccs[0]
-            print('mfccs shape')
-            print(np.shape(mfccs))
-            librosa.display.specshow(mfccs, sr=sr, hop_length=hop_length)
+            mfccs = librosa.feature.mfcc(y=samples, n_fft=self.n_fft, hop_length=self.hop_length, n_mfcc=self.n_mfcc)
+            mfccs = mfccs[0]  # Remove first dimension (mono channel)
+            librosa.display.specshow(mfccs, sr=sample_rate, hop_length=self.hop_length)
             plt.xlabel('Time')
             plt.ylabel('MFCC')
             plt.colorbar()
             plt.show()
 
-    def preprocess_data(self):
-        # Take random subset
-        # subset = get_random_subset(self.data)
-
+    def preprocess(self):
         # For now, take first 10 elements as a test
         subset = list(self.data)[0:10]
 
-        hop_length = 512  # number of samples to shift
-        n_fft = 2048  # number of samples per fft (window size)
-        n_mfcc = 13  # standard minimum
-
+        mfcc_path = Path('mfcc')
+        if not mfcc_path.exists():
+            mfcc_path.mkdir(parents=True)
+        
+        print('Processing audio data...')
+        progress_bar = ProgressBar()
         for i, item in enumerate(subset):
             samples, sample_rate, transcript, speaker_id, chapter_id, utterance_id = item
-            samples = samples.numpy()
 
-            mfccs = librosa.feature.mfcc(y=samples, n_fft=n_fft, hop_length=hop_length, n_mfcc=n_mfcc)
-            mfccs = mfccs[0]
+            mfccs = librosa.feature.mfcc(y=samples.numpy(), n_fft=self.n_fft, hop_length=self.hop_length, n_mfcc=self.n_mfcc)
+            mfcc_bands = mfccs[0]
+            mfcc_frames = mfcc_bands.T  # (num_bands, num_frames) -> (num_frames, num_bands)
 
-            print('mfccs')
-            print(mfccs)
+            with h5py.File(f'mfcc/{speaker_id}_{chapter_id}_{utterance_id}.hdf5', 'w') as file:
+                dataset = file.create_dataset('mfccs', data=mfcc_frames)
+                print(dataset)
+                dataset.attrs['speaker_id'] = speaker_id
+                dataset.attrs['chapter_id'] = chapter_id
+                dataset.attrs['utterance_id'] = utterance_id
+                dataset.attrs['sample_rate'] = sample_rate
 
-            # Export MFCCs along with other features to file for training
+            progress_bar.update(i + 1, len(subset))
+        
+    def read_preprocessed_data(self):
+        for root, directories, files in os.walk('mfcc'):
+            for file in files:
+                with h5py.File(f'mfcc/{file}', 'r') as file_data:
+                    mfccs_dataset = file_data['mfccs']
+                    print(mfccs_dataset)
+                    for attr in list(mfccs_dataset.attrs):
+                        print(f'{attr}: {mfccs_dataset.attrs[attr]}')
 
+    def train(self):
+        # Load data (multiprocessing)
+
+        # Architecture (LSTM -> GRU)
+        # - GRU Layer (64)
+        # - GRU Layer (64)
+        # - Dense Layer
+        # - Dropout Layer (to prevent overfitting)
+        # - Dense Layer (softmax output)
+        pass
 
 def main():
     args = sys.argv[1:]
     rnn = SpeechToTextRNN()
-    if '--show-data' in args:
+    if '--display' in args:
         rnn.show_test_data(
             waveform=True if '--waveform' in args else False,
             spectrogram=True if '--spectrogram' in args else False,
             mfcc=True if '--mfcc' in args else False,
             play=True if '--play' in args else False)
     if '--preprocess' in args:
-        rnn.preprocess_data()
-        
+        rnn.preprocess()
+    if '--read-mfcc' in args:
+        rnn.read_preprocessed_data()
 
 if __name__ == '__main__':
     main()
-
-
-def doc_test(blarg: str):
-    """
-    :param blarg: A random string
-    :return: Returns the string 'yo'
-
-    Subtitle 1
-    ----------
-    What's upppppp
-
-    - List 1
-    - Item 2
-
-    >>> import test.blarg as test
-    """
-    librosa.core.st
-
-    return 'yo'
