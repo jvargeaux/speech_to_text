Implement attention mask & fix data shapes
* Implement training & label smoothing

* Clean up preprocess file

src/preprocess.py
@@ -13,6 +13,7 @@ import h5py
 import os
 import multiprocessing as mp
 import time
+from enum import Enum
 
 
 device = 'cpu'
@@ -21,6 +22,17 @@ if torch.cuda.is_available():
 elif torch.backends.mps.is_available():
     device = 'mps'
 device = torch.device(device)
+
+
+class SPLITS(Enum):
+    DEV_CLEAN = 'dev-clean'
+    DEV_OTHER = 'dev-other'
+    TRAIN_CLEAN_100 = 'train-clean-100'
+    TRAIN_CLEAN_360 = 'train-clean-360'
+    TRAIN_OTHER_500 = 'train-other-500'
+    TEST_CLEAN = 'test-clean'
+    TEST_OTHER = 'test-other'
+
 
 class ProgressBar():
     def __init__(self):
@@ -38,76 +50,29 @@ class ProgressBar():
             print()
 
 
-class SpeechToTextRNN(nn.Module):
-    """
-        GRU-based Recurrent Neural Network. Extends torch.nn.Module.
-
-        Architecture
-        - GRU Layer (64)
-        - GRU Layer (64)
-        - Dense Layer
-        - Dropout Layer (to prevent overfitting)
-        - Dense Layer (softmax output)
-
-        Parameters
-        ---
-            input_size: size of the input data
-            hidden_size: size of the GRU hidden state
-            output_size: size of the output data
-            num_layers: number of GRU layers
-            drop_probability: drop probability of final Dropout layer (if non-zero)
-
-        Returns
-        ---
-            network (object): The neural network (GRU)
-    """
-    def __init__(self, input_size, hidden_size=64, output_size=16, num_layers=2, drop_probability=0.2):
-        super().__init__()
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-
-        # Network Layers
-        self.gru = nn.GRU(input_size,
-                          hidden_size=hidden_size,
-                          num_layers=num_layers,
-                          batch_first=True,
-                          dropout=drop_probability)
-        self.fc = nn.Linear(hidden_size, output_size)
-        self.relu = nn.ReLU()
-
-    def forward(self, x, h):
-        output, hidden_state = self.gru(x, h)
-        output = self.fc(self.relu(output[:,-1]))
-        return output, hidden_state
-
-    def init_hidden(self, batch_size):
-        weight = next(self.parameters()).data
-        hidden = weight.new(self.num_layers, batch_size, self.hidden_size).zero_().to(device)
-        return hidden
-
-
 class SpeechToTextTrainer():
-    def __init__(self):
-        # Set hyperparameters
-        self.batch_size = 8
-        self.hidden_size = 256
-        self.drop_probability = 0.2
-        self.learn_rate = 0.001
-        self.num_epochs = 2
-
+    '''
+    Arguments
+    - dataset_url: Name of dataset split, check SPLITS enum for options
+    '''
+    def __init__(self, dataset_url: str = 'DEV_CLEAN'):
         # Set MFCC meta parameters
         self.hop_length = 512  # number of samples to shift
         self.n_fft = 2048  # number of samples per fft (window size)
         self.n_mfcc = 13  # standard minimum
 
-        self.download_dataset()
+        self.data = None
+        self.download_dataset(url=dataset_url)
 
-    def download_dataset(self):
+    def download_dataset(self, url: str):
+        if url not in [item.value for item in SPLITS]:
+            print('Invalid dataset url. Check SPLITS enum for options.')
+            return
         data_path = Path('data')
         if not data_path.exists():
             data_path.mkdir(parents=True)
         # Updated dataset from LibriLightLimited -> LibriSpeech (same format)
-        data = torchaudio.datasets.LIBRISPEECH(root=data_path, url='dev-clean', download=True)
+        data = torchaudio.datasets.LIBRISPEECH(root=data_path, url=url, download=True)
         self.data = data
 
     def show_test_data(self, waveform=False, spectrogram=False, mfcc=False, play=False):
@@ -225,65 +190,15 @@ class SpeechToTextTrainer():
 
         # return padded_batch, torch.cat(target_batch, dim=0).reshape(len(sample_batch))
 
-    def train(self):
-        # Load data
-        print('Loading MFCC data...')
-        mfcc_data = []
-        for root, directories, files in os.walk('mfcc'):
-            for file in files:
-                with h5py.File(f'mfcc/{file}', 'r') as file_data:
-                    mfcc_data.append([file_data['mfccs'][:],
-                                     file_data['mfccs'].attrs['sample_rate'],
-                                     file_data['mfccs'].attrs['speaker_id']])
-        print('MFCC data loaded.')
-
-        if device == 'cpu':
-            # Multiprocess
-            num_processes = mp.cpu_count() - 1 or 1
-            print('Using number of processes:', num_processes)
-            # pool = mp.Pool(processes=num_processes)
-
-        # Build network model
-        train_data = mfcc_data
-        train_loader = DataLoader(train_data, shuffle=True, batch_size=self.batch_size, drop_last=True, collate_fn=self.collate)
-        batch = next(iter(train_loader))
-        item = batch[0]
-        mfccs = item[0]
-        print('Shape of mfccs:', mfccs.shape)
-        input_size = mfccs.shape[1]
-        print('Input size:', input_size)
-        model = SpeechToTextRNN(input_size=input_size,
-                              hidden_size=self.hidden_size,
-                              output_size=64,
-                              num_layers=2,
-                              drop_probability=self.drop_probability)
-        model.to(device)
-
-        # Define loss & optimizer
-        criterion = nn.MSELoss()
-        optimizer = torch.optim.Adam(model.parameters(), lr=self.learn_rate)
-
-        model.train()
-        print('Training network...')
-
-        # Training
-        for epoch in range(0, self.num_epochs):
-            start_time = time.perf_counter()
-
-            h = model.init_hidden(self.batch_size)
-            avg_loss = 0
-            # Perform forward and calculate loss
-
-            end_time = time.perf_counter()
-            print(f'Epoch: {epoch + 1}/{self.num_epochs} | Loss: {avg_loss} | Time Elapsed: {end_time - start_time}')
-
-        print('Done training.')
-
 
 def main():
     print(f'device: {device}')
     args = sys.argv[1:]
-    trainer = SpeechToTextTrainer()
+
+    dataset_url = SPLITS.DEV_CLEAN.value
+    print('Dataset split:', dataset_url)
+    trainer = SpeechToTextTrainer(dataset_url=dataset_url)
+
     if '--display' in args:
         trainer.show_test_data(
             waveform=True if '--waveform' in args else False,

src/trainer.py
@@ -2,6 +2,8 @@ import os
 import h5py
 import time
 import torch
+from torch import nn, Tensor
+from torch.utils.data import DataLoader
 
 from torchtext.data.utils import get_tokenizer
 from torchtext.vocab import build_vocab_from_iterator
@@ -9,8 +11,9 @@ from src.transformer import Transformer
 
 
 class Vocabulary():
-    def __init__(self, batch):
+    def __init__(self, batch, device):
         # Go through all batches of data, and expand the vocabulary
+        self.device = device
         self.tokenizer = get_tokenizer('basic_english')
         self.vocab = None
         self.vocab_size = 0
@@ -25,68 +28,139 @@ class Vocabulary():
 
     def get_tensor_from_sequence(self, source_sequence: str):
         # Convert tokens -> vocab indices -> tensor
-        return torch.tensor(self.vocab(self.tokenizer(source_sequence)), dtype=torch.long)
+        return torch.tensor(self.vocab(self.tokenizer(source_sequence)), dtype=torch.long).to(self.device)
 
     def get_word_from_index(self, index: int):
         return self.vocab.lookup_token(index)
 
 
+class LabelSmoothing(nn.Module):
+    def __init__(self, smoothing: float = 0.0):
+        super().__init__()
+        self.criterion = nn.KLDivLoss(reduction='sum')  # size_average=False will be deprecated
+        self.smoothing = smoothing
+        self.confidence = 1.0 - smoothing
+        self.true_dist = None
+
+    def forward(self, x: Tensor, target_sequence: Tensor):
+        '''
+        x: Model output, shape (batch_size, sequence_length, vocab_size)
+        '''
+        true_dist = x.data.clone()[0]
+        size = x.size(2)
+        true_dist.fill_(self.smoothing / (size - 2))
+        true_dist.scatter_(1, target_sequence.data.unsqueeze(1), self.confidence)
+        true_dist[:, 0] = 0
+        mask = torch.nonzero(target_sequence.data == 0)
+        if mask.dim() > 0:
+            true_dist.index_fill_(0, mask.squeeze(), 0.0)
+        self.true_dist = true_dist
+        # print('True dist:', true_dist)
+        return self.criterion(x, true_dist)
+
+
 class Trainer():
-    def __init__(self, d_model: int, num_layers: int, dropout: float,
-                 num_heads: int, max_length: int, device: str):
+    def __init__(self, d_model: int, num_layers: int, dropout: float, num_heads: int,
+                 max_length: int, device):
+        self.device = device
         self.d_model = d_model
         self.num_layers = num_layers
         self.dropout = dropout
         self.num_heads = num_heads
         self.max_length = max_length
-        self.device = device
 
-    def train(self, num_epochs: int):
+    def collate(self, batch):
+        # Pad batches?
+        return batch
+
+        # sample_batch, target_batch = [], []
+        # for sample, target in batch:
+        #     sample_batch.append(sample)
+        #     target_batch.append(target)
+
+        # padded_batch = pad_sequence(sample_batch, batch_first=True)
+        # padded_to = list(padded_batch.size())[1]
+        # padded_batch = padded_batch.reshape(len(sample_batch), padded_to, 1)
+
+        # return padded_batch, torch.cat(target_batch, dim=0).reshape(len(sample_batch))
+
+    def train(self, num_epochs: int, batch_size: int, optimizer, learning_rate):
         # Import preprocessed mfcc data
-        inputs = []
+        data = []
         for _, _, files in os.walk('mfcc'):
-            file = files[0]
-            with h5py.File(f'mfcc/{file}', 'r') as file_data:
-                inputs.append([file_data['mfccs'][:],
-                            file_data['mfccs'].attrs['sample_rate'],
-                            file_data['mfccs'].attrs['transcript'],
-                            file_data['mfccs'].attrs['speaker_id']])
-        sample_input = inputs[0]
-        mfccs = torch.tensor(sample_input[0])  # mfccs
-        transcript = sample_input[2]  # transcript
-        print(mfccs.shape)
-        print('Number of audio chunks:', mfccs.shape[0])
-        print('MFCC dimension:', mfccs.shape[1])
-        print('Target sequence:', transcript)
-        print('Target sequence length:', len(transcript.split()))
+            for file in files:
+                with h5py.File(f'mfcc/{file}', 'r') as file_data:
+                    data.append([file_data['mfccs'][:],
+                                file_data['mfccs'].attrs['sample_rate'],
+                                file_data['mfccs'].attrs['transcript'],
+                                file_data['mfccs'].attrs['speaker_id']])
+        print('Files:', len(data))
+
+        # sample_input = data[0]
+        # mfccs = torch.tensor(sample_input[0])  # mfccs
+        # transcript = sample_input[2]  # transcript
+        # print(mfccs.shape)
+        # print('Number of audio chunks:', mfccs.shape[0])
+        # print('MFCC dimension:', mfccs.shape[1])
+        # print('Target sequence:', transcript)
+        # print('Target sequence length:', len(transcript.split()))
 
         # Build vocabulary from all transcripts
-        all_sequences = [transcript]
-        vocabulary = Vocabulary(batch=all_sequences)
+        transcripts = [item[2] for item in data]
+        vocabulary = Vocabulary(batch=transcripts, device=self.device)
         vocab_size = vocabulary.vocab_size
         print('Model Vocab Size:', vocab_size)
         # all_words = vocab.get_itos()
         print()
 
+        # Prepare training data
+        train_loader = DataLoader(dataset=data, batch_size=batch_size, shuffle=True, drop_last=True, collate_fn=self.collate)
+        num_steps = len(train_loader)
+
         # Build model
-        model = Transformer(vocab_size=vocab_size, d_model=self.d_model, dropout=self.dropout,
+        self.model = Transformer(vocab_size=vocab_size, d_model=self.d_model, dropout=self.dropout,
                             num_heads=self.num_heads, max_length=self.max_length, num_layers=self.num_layers,
                             device=self.device).to(self.device)
-        
+
+        optimizer = optimizer(self.model.parameters(), lr=learning_rate, betas=(0.9, 0.98), eps=1e-9)
+
+
 
         for epoch in range(num_epochs):
-            print(f'Epoch: {epoch+1}/{num_epochs}')
+            start = time.time()
+            total_tokens = 0
+            total_loss = 0
+            tokens = 0
 
-        # Set source and target
-        source_mfccs = mfccs
-        target_sequence = vocabulary.get_tensor_from_sequence(transcript)
-        
-        # Train
-        out = model(source_mfccs=source_mfccs, target_sequence=target_sequence)
+            for i, batch in enumerate(train_loader):
+                source_mfccs = [torch.tensor(item[0]).to(self.device) for item in batch]
+                transcripts = [item[2] for item in batch]
+                target_sequences = list(map(vocabulary.get_tensor_from_sequence, transcripts))
 
-        print('\nModel Output:', out.shape)
-        output = out[0][-1]
-        guess_index = torch.argmax(output)
-        print(guess_index)
-        print(vocabulary.get_word_from_index(guess_index))
-        print()
+                # For now, take only first in batch
+                source_mfccs = source_mfccs[0]
+                target_sequence = target_sequences[0]
+
+                # Train
+                out = self.model(source_mfccs=source_mfccs, target_sequence=target_sequence)
+                guess_index = torch.argmax(out[0][-1])
+                prediction = vocabulary.get_word_from_index(guess_index)
+                # print(f'Prediction: {guess_index} | {prediction}')
+
+                criterion = LabelSmoothing(smoothing=0.1)
+                target_y = target_sequence[1:]
+                # num_tokens
+                loss = criterion(out, target_sequence)
+
+                loss.backward()
+                optimizer.step()
+                optimizer.zero_grad()
+
+                # print('Target Sequence:', target_sequence)
+                # print('Target Y:', target_y)
+                # print('Criterion input:', out[0].shape, target_sequence.shape)
+                # print('Loss:', loss)
+
+                if i % 50 == 0:
+                    elapsed = time.time() - start
+                    print(f'Epoch: {epoch+1}/{num_epochs} | Step: {i}/{num_steps} | Loss: {loss.item():.4f} | Time: {elapsed:.4f}')

src/transformer.py
@@ -9,11 +9,11 @@ from typing import Optional
 class Logger():
     def __init__(self, debug=True):
         self.debug = debug
-    
+
     def __call__(self, *args):
         if self.debug:
             print(*args)
-    
+
 log = Logger(debug=False)
 
 
@@ -50,8 +50,9 @@ class AudioEmbedder(nn.Module):
       | [o] //
     ```
     '''
-    def __init__(self, embed_dim: int):
+    def __init__(self, embed_dim: int, device):
         super().__init__()
+        self.device = device
         self.embed_dim = embed_dim
         self.conv = nn.Sequential(
             nn.Conv2d(in_channels=1, out_channels=4, kernel_size=5, padding=2),
@@ -70,7 +71,7 @@ class AudioEmbedder(nn.Module):
         # log('Padded:', padded.shape)
         return reshaped
 
-    def embed(self, source: Tensor, target_length: int):
+    def forward(self, source: Tensor, target_length: int):
         log('[Audio Embedder] Source shape:', source.shape)
         log('[Audio Embedder] Target sequence length:', target_length)
         log('[Audio Embedder] Embed dimension:', self.embed_dim)
@@ -79,17 +80,17 @@ class AudioEmbedder(nn.Module):
         # log('Square:', square.shape)
         out = square.unsqueeze(0)  # Add 1 dimension for conv input channel
         out = self.conv(out)  # Shape: (conv_channels, dim_1, dim_2)
-        normalize = nn.LayerNorm(out.shape[-1])
+        normalize = nn.LayerNorm(out.shape[-1]).to(self.device)
         out = normalize(out)
         # log('Conv & Norm:', out.shape)
         out = out.view(-1, out.shape[-1]).contiguous()  # (conv_channels*dim_1, dim_2)
         # log('Flatten:', out.shape)
-        linear1 = nn.Linear(in_features=out.shape[1], out_features=target_length)  # dim_2 -> target_length
+        linear1 = nn.Linear(in_features=out.shape[1], out_features=target_length).to(self.device)  # dim_2 -> target_length
         out = linear1(out)  # (conv_channels*dim_1, target_length)
         # log('Linear1:', out.shape)
         out = out.transpose(0, 1).contiguous()  # (target_length, conv_channels*dim_1)
         # log('Flip:', out.shape)
-        linear2 = nn.Linear(in_features=out.shape[1], out_features=self.embed_dim)  # conv_channels*dim_1 -> embed_dim
+        linear2 = nn.Linear(in_features=out.shape[1], out_features=self.embed_dim).to(self.device)  # conv_channels*dim_1 -> embed_dim
         out = linear2(out)  # (target_length, embed_dim)
         # log('Linear2:', out.shape)
         return out
@@ -214,8 +215,9 @@ class PositionalEncoding(nn.Module):
 
 
 class MultiheadAttention(nn.Module):
-    def __init__(self, d_model: int, num_heads: int, dropout: float = 0.1):
+    def __init__(self, d_model: int, num_heads: int, dropout: float, device):
         super().__init__()
+        self.device = device
         self.d_model = d_model
         self.num_heads = num_heads
         self.head_dim = d_model // num_heads
@@ -230,6 +232,7 @@ class MultiheadAttention(nn.Module):
         # Compute "Scaled Dot Product Attention"
         d_k = query.size(-1)
         scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
+        log('[Multihead Attention] Scores (attention) shape:', scores.shape)
         if mask is not None:
             scores = scores.masked_fill(mask==0, -1e9)
         prob = softmax(scores, dim=-1)
@@ -238,17 +241,15 @@ class MultiheadAttention(nn.Module):
         return torch.matmul(prob, value), prob
 
     def forward(self, query, key, value, mask = None):
-        log('[Multihead Attention]', 'Shape of Q/K/V:', value.shape)
-        N = query.shape[0]  # Batch size
-        log('[Multihead Attention]', 'Num heads:', self.num_heads)
-        log('[Multihead Attention]', 'Head dimension:', self.head_dim)
-        log('[Multihead Attention]', 'Mask:', mask)
+        N = query.shape[0]  # batch size
+        log('[Multihead Attention] Shape of Q/K/V:', value.shape)
+        log('[Multihead Attention] Num heads:', self.num_heads)
+        log('[Multihead Attention] Head dimension:', self.head_dim)
+        log('[Multihead Attention] Batch size:', N)
 
         if mask is not None:
             mask = mask.unsqueeze(1)
-            pass
-        num_batches = 1
-        # num_batches = query.size(0)
+            log('[Multihead Attention] Mask shape:', mask.shape)
 
         # Linear projections
         value = self.linear(value)
@@ -260,18 +261,21 @@ class MultiheadAttention(nn.Module):
         # Reshape: (sequence_length, embed_dim) -> (batch_size, query_length, num_heads, head_dim)
         # Then: (batch_size, query_length, num_heads, head_dim) ->
         #       (query_length, batch_size, num_heads, head_dim)
-        value = value.view(num_batches, -1, self.num_heads, self.head_dim).transpose(1, 0)
-        key = key.view(num_batches, -1, self.num_heads, self.head_dim).transpose(1, 0)
-        query = query.view(num_batches, -1, self.num_heads, self.head_dim).transpose(1, 0)
-        log('[Multihead Attention]', 'Shape of Q/K/V after reshape:', value.shape)
+        value = value.view(N, -1, self.num_heads, self.head_dim).transpose(1, 2)
+        key = key.view(N, -1, self.num_heads, self.head_dim).transpose(1, 2)
+        query = query.view(N, -1, self.num_heads, self.head_dim).transpose(1, 2)
+        log('[Multihead Attention] Shape of sliced Q/K/V:', value.shape)
 
         # 2) Apply attention on all projected vectors
         x, self.attention = self._attention(query, key, value, mask=mask, dropout=self.dropout)
+        log('[Multihead Attention] Shape of attention output:', x.shape, self.attention.shape)
 
         # 3) Concat (flatten last 2 dimensions)
         # Essentially, undo the reshape we did earlier
-        out = x.transpose(0, 1).contiguous().view(num_batches, -1, self.num_heads * self.head_dim)
+        # Final shape: (batch_size, )
+        out = x.transpose(0, 1).contiguous().view(N, -1, self.num_heads * self.head_dim)
         out = self.linear(out)
+        log('[Multihead Attention] Shape of concat (flattened) attention:', out.shape)
         return out
 
 
@@ -303,12 +307,12 @@ class Encoder(nn.Module):
     Output
     ```
     '''
-    def __init__(self, d_model: int, num_layers: int, dropout: float, num_heads: int, forward_expansion: int = 4,
+    def __init__(self, d_model: int, num_layers: int, dropout: float, num_heads: int, device, forward_expansion: int = 4,
                  mask: Optional[Tensor] = None):
         super().__init__()
         self.d_model = d_model
         self.num_layers = num_layers
-        self.attention = MultiheadAttention(d_model, num_heads)
+        self.attention = MultiheadAttention(d_model=d_model, num_heads=num_heads, dropout=dropout, device=device)
         self.norm1 = nn.LayerNorm(d_model)
         self.norm2 = nn.LayerNorm(d_model)
         self.feed_forward = nn.Sequential(
@@ -370,12 +374,12 @@ class Decoder(nn.Module):
     Output Probabilities
     ```
     '''
-    def __init__(self, d_model: int, num_layers: int, dropout: float, num_heads: int, forward_expansion: int = 4,
+    def __init__(self, d_model: int, num_layers: int, dropout: float, num_heads: int, device, forward_expansion: int = 4,
                  mask: Optional[Tensor] = None):
         super().__init__()
         self.d_model = d_model
         self.num_layers = num_layers
-        self.attention = MultiheadAttention(d_model, num_heads)
+        self.attention = MultiheadAttention(d_model=d_model, num_heads=num_heads, dropout=dropout, device=device)
         self.norm1 = nn.LayerNorm(d_model)
         self.norm2 = nn.LayerNorm(d_model)
         self.norm3 = nn.LayerNorm(d_model)
@@ -422,15 +426,52 @@ class Transformer(nn.Module):
         super().__init__()
         self.d_model = d_model
         self.device = device
-        self.audio_embedder = AudioEmbedder(embed_dim=d_model)
+        self.audio_embedder = AudioEmbedder(embed_dim=d_model, device=device)
         self.word_embeddings = WordEmbedder(vocab_size=vocab_size, d_model=d_model)
         self.positional_encoding = PositionalEncoding(d_model=d_model, dropout=dropout, max_length=max_length)
-        self.encoder = Encoder(d_model=d_model, dropout=dropout, num_heads=num_heads, num_layers=num_layers)
-        self.decoder = Decoder(d_model=d_model, dropout=dropout, num_heads=num_heads, num_layers=num_layers)
+        self.encoder = Encoder(d_model=d_model, dropout=dropout, num_heads=num_heads, num_layers=num_layers, device=device)
+        self.decoder = Decoder(d_model=d_model, dropout=dropout, num_heads=num_heads, num_layers=num_layers, device=device)
         self.linear = nn.Linear(d_model, vocab_size)
 
+    def make_mask(self, source: Tensor):
+        # Encoder output shape: (batch_size, sequence_length, d_model)
+        log('[Mask] Encoder output (source) shape:', source.shape)
+        N, target_length, _ = source.shape
+
+        attention_shape = (1, target_length, target_length)  # (1, sequence_length, sequence_length)
+        mask = torch.tril(torch.ones(attention_shape)).to(self.device)
+        log('[Mask] Attention mask shape:', mask.shape)
+        # Final mask shape at multihead attention will be: (batch_size, 1, sequence_length, sequence_length)
+
+        # Calculate tokens?
+
+        # pad = 0
+        # target = source[:, :-1]
+        # target_y = source[:, 1:]
+        # # New target (target_y) shape: (batch_size, sequence_length - 1, d_model)
+        # print('[Mask] Target (Target_y) shape:', target.shape)
+
+        # target_mask = (target != pad)
+        # # target_mask = (target != pad).unsqueeze(-2)
+        # print('[Mask] Target Mask shape:', target_mask.shape)
+        # print('[Mask] Target Mask:', target_mask)
+        # target_mask = target_mask & mask.type_as(target_mask.data)
+        # print('[Mask] Target Mask:', target_mask)
+
+        # num_tokens = (target_y != pad).data.sum()
+        # print('[Mask] Num tokens:', num_tokens)
+
+        return mask
+
     def forward(self, source_mfccs: Tensor, target_sequence: Tensor):
-        source = self.audio_embedder.embed(source_mfccs, len(target_sequence))
+        '''
+        Source shape: (batch_size, num_chunks, mfcc_length)
+        Target shape: (batch_size, sequence_length)
+        '''
+
+        # NOTE: Need to implement batching for audio embedder, word embedder, and positional encoding
+
+        source = self.audio_embedder(source_mfccs, len(target_sequence))
         log('[Transformer] Source shape:', source.shape)
         log('[Transformer] Audio Embedding Output:', source.shape)
         log('[Transformer] Size of word embedding matrix:', self.word_embeddings.embeddings_lut)
@@ -439,15 +480,22 @@ class Transformer(nn.Module):
         # Encoder
         source = self.positional_encoding(source)
         log('[Transformer] Source shape after positional encoding:', source.shape)
+        source = source.unsqueeze(0)  # Add batches: (sequence_length, d_model) -> (batch_size, sequence_length, d_model)
+        log('[Transformer] Shape of encoder input:', source.shape)
         encoder_out = self.encoder(x=source, mask=None)
         log('[Transformer] Shape of encoder output:', encoder_out.shape)
+
+        target_mask = self.make_mask(source=encoder_out)
+        log('[Transformer] Target mask shape:', target_mask.shape)
 
         # Decoder
         target = self.word_embeddings(target_sequence)
         log('[Transformer] Target shape after word embeddeding:', target.shape)
         target = self.positional_encoding(target)
         log('[Transformer] Target shape after positional encoding:', target.shape)
-        decoder_out = self.decoder(x=target, encoder_out=encoder_out, source_mask=None, target_mask=None)
+        target = target.unsqueeze(0)  # Add batches: (sequence_length, d_model) -> (batch_size, sequence_length, d_model)
+        log('[Transformer] Shape of decoder input:', target.shape)
+        decoder_out = self.decoder(x=target, encoder_out=encoder_out, source_mask=None, target_mask=target_mask)
         log('[Transformer] Shape of decoder output:', decoder_out.shape)
 
         out = log_softmax(self.linear(decoder_out), dim=-1)

train.py
@@ -1,14 +1,41 @@
+import os
+import torch
+from torch import nn
 from src.trainer import Trainer
 
 
+# print(os.getenv('PYTORCH_ENABLE_MPS_FALLBACK'))
+
+# Set to avoid this error:
+# NotImplementedError: The operator 'aten::index_fill_.int_Scalar' is not currently
+# implemented for the MPS device. If you want this op to be added in priority during
+# the prototype phase of this feature, please comment on
+# https://github.com/pytorch/pytorch/issues/77764. As a temporary fix, you can set
+# the environment variable `PYTORCH_ENABLE_MPS_FALLBACK=1` to use the CPU as a fallback
+# for this op. WARNING: this will be slower than running natively on MPS.
+
 def main():
-    # Hyperparameters
-    d_model = 32  # embed_size
-    num_heads = 4
+    print()
+    device = 'cpu'
+    if torch.cuda.is_available():
+        device = 'cuda'
+    # elif torch.backends.mps.is_available():  # some functions not impemented in mps
+    #     device = 'mps'
+    device = torch.device(device)
+    print('Device:', device)
+
+    # Model Hyperparameters
+    d_model = 256  # embed_size
+    num_heads = 8
     dropout = 0.1
     max_length = 5000
-    num_layers = 2
-    device = 'cpu'
+    num_layers = 6
+
+    # Training
+    num_epochs = 4
+    batch_size = 1
+    optimizer = torch.optim.Adam
+    learning_rate = 0.01
 
     trainer = Trainer(d_model=d_model,
                       num_layers=num_layers,
@@ -16,7 +43,10 @@ def main():
                       num_heads=num_heads,
                       max_length=max_length,
                       device=device)
-    trainer.train(num_epochs=2)
+    trainer.train(num_epochs=num_epochs,
+                  batch_size=batch_size,
+                  optimizer=optimizer,
+                  learning_rate=learning_rate)
 
 if __name__ == '__main__':
     main()