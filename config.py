afb670f12c8b90deb60929919a03ea6a8503237b
Jimmy Vargo <james@ayo.tokyo>
2023-12-08 22:32:08 +0900

    Update config

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ class Config:
     MODEL_SAMPLE_RATE = 16000
     HOP_LENGTH = 160  # number of samples to shift  |  16000 / 160 = 100 per second
     N_FFT = 320  # number of samples per fft (window size)
-    MFCC_DEPTH = 80
+    MFCC_DEPTH = 64
 
     # Model
     D_MODEL = 512
     NUM_HEADS = 8
     DROPOUT = 0.1
-    MAX_SOURCE_LENGTH = 1800  # 50/second => 30s total | max: 1748
+    MAX_SOURCE_LENGTH = 4000  # 50/second => 30s total | max: 3496
     MAX_TARGET_LENGTH = 200   #                        | max: 86
     MAX_VOCAB_SIZE = 40000
     NUM_LAYERS = 6
@@ -20,10 +20,10 @@ class Config:
     # Training
     NUM_EPOCHS = 40
     LR = 3e-4
-    LR_GAMMA = 0.9998
+    LR_GAMMA = 0.9999
     LR_MIN = 1e-7
     WEIGHT_DECAY = 1e-4
-    NUM_WARMUP_STEPS = 200
+    NUM_WARMUP_STEPS = 1000
     COOLDOWN = 0  # seconds every step, None = no cooldown
     SPLIT_TRAIN = SPLITS.TRAIN_CLEAN_100.value
     SPLIT_TEST = SPLITS.TEST_CLEAN.value
