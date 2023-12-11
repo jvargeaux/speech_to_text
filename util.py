10553ef0a4759d561e700e66831191d5d15e3ed3
Jimmy Vargo <james@ayo.tokyo>
2023-12-11 22:43:52 +0900

    Add time to progress bar

--- util.py
+++ util.py
@@ -1,15 +1,22 @@
+import time
 
 class ProgressBar():
     def __init__(self):
         self.length = 30
         self.fill = '█'
         self.empty = '-'
+        self.start = time.time()
         print(f'Progress: {self.empty * self.length} | 0/0 | {0:.1f}%', end='\r')
 
     def update(self, filled, total):
         percentage = min([filled, total]) / total
         filled_length = int(percentage * self.length)
         empty_length = self.length - filled_length
-        print(f'Progress: {self.fill * filled_length}{self.empty * empty_length} | {filled}/{total} | {(percentage * 100):.1f}%', end='\r')
+        elapsed = time.time() - self.start
+        final = elapsed / percentage
+        remaining = final - elapsed
+        print(
+            f'Progress: {self.fill * filled_length}{self.empty * empty_length} | {filled}/{total} | {(percentage * 100):.1f}% | '
+            f'{(elapsed // 60):02.0f}:{(elapsed % 60):02.0f}, {(remaining // 60):02.0f}:{(remaining % 60):02.0f} remaining', end='\r')
         if percentage >= 1:
             print()
